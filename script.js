7dc3224d133a0c4396d93b626fdc1f8de6ce8fe5
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-01 23:44:09 +0530

    Add screen1 and screen2 variables

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.onload = function() {
     const emailInput = document.getElementById('email');
     const emailError = document.getElementById('emailError');
+    const screen1 = document.querySelector('.screen1');
+    const screen2 = document.querySelector('.screen2');
 
     emailInput.oninvalid = function(event) {
         event.preventDefault();
