1e027d97e80a67011f78e48edb142ad11a096775
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-02 23:46:45 +0530

    Update dismiss button logic in index.html and script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     const emailInput = document.getElementById('email');
     const emailError = document.getElementById('emailError');
+    const dismissBtn = document.querySelector('.dismiss-btn');
 
     emailInput.oninvalid = function(event) {
         event.preventDefault();
@@ -11,8 +12,14 @@ window.onload = function() {
         emailError.textContent = '';
     }
     document.querySelector('.btn').addEventListener('click', function(event) {
-        // event.preventDefault();
-        document.getElementById('screen1').style.display = 'none';
-        document.getElementById('screen2').style.display = 'flex';
+        event.preventDefault();
+        document.querySelector('.screen1').style.display = 'none';
+        document.querySelector('.screen2').style.display = 'flex';
     });
+    dismissBtn.addEventListener('click', function () {
+        // Dismiss button logic, for example, hide the success message
+        document.querySelector('.screen2').style.display = 'none';
+        // Optionally, you can reset the form or perform other actions here
+        document.querySelector('.screen1').style.display = 'flex';
+      });
 }
